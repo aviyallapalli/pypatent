74233725d276a390c6a7bfefa03251862c5a1727
Dmitry Shabanov <shabanovdmitryv@gmail.com>
2017-02-17 16:38:05 +0300

    conll format visualization via graphviz

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,23 @@
 from docs.conf import Config
 
-print(Config.tt_model)
+from pypatent.parser.xml_reader import read_element
+from pypatent.parser.segment import segment
+from pypatent.parser.treetagger import TreeTagger
+from pypatent.parser.conll import conll_eng as conll
+from pypatent.parser.maltparser import MaltParser
+
+from pypatent.visualize import tree_to_png, parse_conll_text, build_graph
+
+filepath = "/home/john/Projects/1.xml"
+
+tt = TreeTagger(Config.tt_dir + Config.tt_bin, Config.tt_dir + Config.tt_model)
+mp = MaltParser(Config.mp_dir, Config.mp_jar, Config.mp_model)
+
+text = read_element(filepath, "claims")
+text = segment(text)
+text = tt.tag(text)
+text = conll(text)
+text = mp.parse(text)
+
+tree = parse_conll_text(text)[0]
+tree_to_png(tree, "/home/john/tree.png")
